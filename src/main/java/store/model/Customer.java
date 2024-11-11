c8be6022ab4f38118cc83f72ddcee440b9b2d4b9
RIWON <121172065+awrion3@users.noreply.github.com>
2024-11-11 13:22:12 +0900

    refactor(customer): add static final field

--- src/main/java/store/model/Customer.java
+++ src/main/java/store/model/Customer.java
@@ -3,6 +3,9 @@ package store.model;
 import store.view.Exception;
 
 public class Customer {
+    private static final String POSITIVE_RESPONSE = "Y";
+    private static final String NEGATIVE_RESPONSE = "N";
+
     private final String response;
 
     public Customer(String response) {
@@ -11,19 +14,19 @@ public class Customer {
     }
 
     private void validateResponse(String response) {
-        if (!response.contains("Y") && !response.contains("N")) {
+        if (!response.contains(POSITIVE_RESPONSE) && !response.contains(NEGATIVE_RESPONSE)) {
             Exception.getInvalidInput();
         }
-        if (response.contains("Y") && !response.equals("Y")) {
+        if (response.contains(POSITIVE_RESPONSE) && !response.equals(POSITIVE_RESPONSE)) {
             Exception.getInvalidInput();
         }
-        if (response.contains("N") && !response.equals("N")) {
+        if (response.contains(NEGATIVE_RESPONSE) && !response.equals(NEGATIVE_RESPONSE)) {
             Exception.getInvalidInput();
         }
     }
 
     public boolean isResponseYes() {
-        boolean isResponseYes = response.equals("Y");
+        boolean isResponseYes = response.equals(POSITIVE_RESPONSE);
         return isResponseYes;
     }
 }
