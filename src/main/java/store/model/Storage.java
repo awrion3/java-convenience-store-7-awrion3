c6dd8691ad8696550c633368931d79e36a5c56d6
RIWON <121172065+awrion3@users.noreply.github.com>
2024-11-11 13:38:02 +0900

    refactor(storage): add static final field

--- src/main/java/store/model/Storage.java
+++ src/main/java/store/model/Storage.java
@@ -12,14 +12,19 @@ public class Storage {
     private static final String NO_RESOURCE_FILE = "[ERROR] 리소스 파일이 존재하지 않습니다.";
     private static final String NO_RESOURCE_ITEM = "[ERROR] 리소스 내용이 존재하지 않습니다.";
 
+    private static final String PRODUCTS_FILENAME = "products.md";
+    private static final String PROMOTIONS_FILENAME = "promotions.md";
+    private static final String FIRST_ROW_START_POINT = "name";
+    private static final String DELIMITER = ",";
+
     private final List<List<String>> resourceProducts;
     private final List<List<String>> resourcePromotions;
 
     public Storage() {
         resourceProducts = new ArrayList<>();
-        readFromResource("products.md");
+        readFromResource(PRODUCTS_FILENAME);
         resourcePromotions = new ArrayList<>();
-        readFromResource("promotions.md");
+        readFromResource(PROMOTIONS_FILENAME);
     }
 
     private void readFromResource(String fileName) {
@@ -36,7 +41,7 @@ public class Storage {
     private void readResourceByLine(BufferedReader reader, String fileName) throws IOException {
         String resourceLine;
         while ((resourceLine = reader.readLine()) != null) {
-            String[] resourceSplitLine = resourceLine.split(",");
+            String[] resourceSplitLine = resourceLine.split(DELIMITER);
             List<String> resourceItems = Arrays.asList(resourceSplitLine);
 
             updateResourceListByLine(resourceItems, fileName);
@@ -47,17 +52,17 @@ public class Storage {
         if (isFirstRow(resourceItems)) {
             return;
         }
-        if (fileName.equals("products.md")) {
+        if (fileName.equals(PRODUCTS_FILENAME)) {
             resourceProducts.add(resourceItems);
         }
-        if (fileName.equals("promotions.md")) {
+        if (fileName.equals(PROMOTIONS_FILENAME)) {
             resourcePromotions.add(resourceItems);
         }
     }
 
     private boolean isFirstRow(List<String> resourceItems) {
         String category = resourceItems.getFirst();
-        return category.equals("name");
+        return category.equals(FIRST_ROW_START_POINT);
     }
 
     private void validateResourceFile() {
